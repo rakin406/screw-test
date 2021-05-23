0150c2cfbbd8b46ca1517d5f8bfe3fd85d68bd68
rakin406 <rakinhacker@gmail.com>
2021-05-23 09:24:20 +0600

    Image to text

--- screw-test.py
+++ screw-test.py
@@ -1,1 +1,13 @@
 #!/usr/bin/env python3
+"""
+Ultra cheating program
+"""
+
+try:
+    from PIL import Image
+except ImportError:
+    import Image
+import pytesseract
+
+# Simple image to string
+print(pytesseract.image_to_string(Image.open("test.png")))
