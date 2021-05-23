cfbec7b62c72bffdebdb3aedb4b05391abdb3824
rakin406 <rakinhacker@gmail.com>
2021-05-23 10:31:07 +0600

    Analyze pdf too

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # screw-test
 An easy cheating program I made for my **dumb** friends. Find mark schemes from
-given question paper picture. Cheat or study? I prefer *both*. Nerds be like -
-"bUt ChEaTiNg WoNt HeLp YoU iN o LeVels" "hOnEsTy Is ThE bEsT pOlIcY", no one
-cares lol.
+given question paper picture or pdf. Cheat or study? I prefer *both*. Nerds be
+like - "bUt ChEaTiNg WoNt HeLp YoU iN o LeVels" "hOnEsTy Is ThE bEsT pOlIcY",
+no one cares lol.
 
 As a wise man once said:
 
--- screw-test.py
+++ screw-test.py
@@ -1,23 +1,27 @@
 #!/usr/bin/env python3
 """
 Ultra cheating program I made for my dumb friends.
-Usage: ./screw-test.py <image>
+Usage: ./screw-test.py <image/pdf>
 Example: ./screw-test.py question-paper.png
 """
 
 import sys
+from pdfminer.high_level import extract_text_to_fp, extract_text
+
 try:
     from PIL import Image
 except ImportError:
     import Image
 import pytesseract
 
-# Get image path
+# Get image or pdf path
 if len(sys.argv) > 1:
-    IMAGE = sys.argv[1]
+    QUESTION = sys.argv[1]
 else:
-    print("Provide question paper image, you lowlife cheater!")
+    print("Provide question paper image or pdf, you lowlife cheater!")
     sys.exit(1)
 
-# Simple image to string
-print(pytesseract.image_to_string(Image.open(IMAGE)))
+if QUESTION.endswith(".pdf"):
+    print(extract_text(QUESTION))
+else:
+    print(pytesseract.image_to_string(Image.open(QUESTION)))
