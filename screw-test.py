a64fc83e2a8c094de0eca82a2caaff148d5c862f
rakin406 <rakinhacker@gmail.com>
2021-05-26 18:05:16 +0600

    h

--- screw-test.py
+++ screw-test.py
@@ -1,18 +1,4 @@
-#!/usr/bin/env python3
-"""
-Ultra cheating program I made for my dumb friends.
-Usage: ./screw-test.py <image/pdf>
-
-Examples:
-./screw-test.py question.jpg
-./screw-test.py paper1.png paper2.pdf
-"""
-
-import sys
 import requests
-from PIL import Image
-import pytesseract
-from pdfminer.high_level import extract_text
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 
@@ -103,62 +89,9 @@ def find_answer(question_url: str) -> str:
     return answer_url
 
 
-def search_and_print(driver, questions):
-    line = 1
-    answer_found = False
-
-    for i in questions:
-        # Links will be unique
-        urls = list(dict.fromkeys(find_question_urls(driver, i)))
-        for question in urls:
-            answer = find_answer(question)
-            if answer is not None:
-                print()
-                print("{}. Question: {}".format(line, question))
-                print("Answer: {}".format(answer))
-
-                if answer_found is False:
-                    answer_found = True
-
-            line += 1
-
-    if answer_found is False:
-        print()
-        print("No answers found lol. RIP")
-
-
-ARGS = len(sys.argv) - 1
-if ARGS == 0:
-    print("Usage: ./screw-test.py <image/pdf>")
-    print("$ ./screw-test.py question.jpg")
-    print("$ ./screw-test.py paper1.png paper2.pdf")
-    sys.exit(1)
-
 # Run headless browser
 options = Options()
 options.headless = True
 driver = webdriver.Firefox(options=options)
-
-for input in range(1, ARGS + 1):
-    # Get text from image or pdf
-    print()
-    question_paper = sys.argv[input]
-    if question_paper.endswith(".pdf"):
-        print("Extracting text from PDF...")
-        text = extract_text(question_paper)
-    else:
-        print("Extracting text from image...")
-        text = pytesseract.image_to_string(Image.open(question_paper))
-
-    if not text:
-        continue
-
-    print("Detecting questions...")
-    questions = detect_questions(text)
-    if not questions:
-        continue
-
-    print("Searching...")
-    search_and_print(driver, questions)
 
 driver.quit()
